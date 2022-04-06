225a60452c2b6fee1fd6a7b86e9320435c89f925
Theodore Ayoub <theodore.ayoub@gmail.com>
2022-04-06 12:48:35 -0700

    configured password length capability

--- script.js
+++ script.js
@@ -26,37 +26,42 @@ var generateBtn = document.querySelector("#generate");
 console.log(passbase)
 var generatePassword = function () {
   // goal of function is to return password
-  var passLength = prompt("password length? ¯\\_(ツ)_/¯");
+  var passLength = prompt("Please enter a number between 8 and 128.");
+
+  if (passLength > 7 && passLength < 129) {
+    var wantnumbers = confirm("numeric characters? ¯\\_(ツ)_/¯");
+    // If user wants numbers in their password, the IF statement below will run.
+    if (wantnumbers) {
+      passbase += numbers;
+    }
+  
+    var specChars = confirm("special characters? ¯\\_(ツ)_/¯");
+    // If user wants special characters in their password, the IF statement below will run.
+    if (specChars) {
+      passbase += specials;
+    }
+  
+    var lowercase = confirm("lowercase characters? ¯\\_(ツ)_/¯");
+    // If user wants lowercase letters in their password, the IF statement below will run.
+    if (lowercase) {
+      passbase += lowers;
+    }
+  
+    var uppercase = confirm("uppercase characters? ¯\\_(ツ)_/¯");
+    // If user wants uppercase characters in their password, the IF statement below will run.
+    if (uppercase) {
+      passbase += uppers;
+    }
+  
+    for (let i = 0; i < passLength; i++) {
+      theChosenOne += passbase.charAt(Math.floor(Math.random()*passbase.length));
+      
+    }
+    return theChosenOne;
+  } else {
+    alert("You did not enter a pasword length between 8 and 128.") 
+  }
  
-  var wantnumbers = confirm("numeric characters? ¯\\_(ツ)_/¯");
-  // If user wants numbers in their password, the IF statement below will run.
-  if (wantnumbers) {
-    passbase += numbers;
-  }
-
-  var specChars = confirm("special characters? ¯\\_(ツ)_/¯");
-  // If user wants special characters in their password, the IF statement below will run.
-  if (specChars) {
-    passbase += specials;
-  }
-
-  var lowercase = confirm("lowercase characters? ¯\\_(ツ)_/¯");
-  // If user wants lowercase letters in their password, the IF statement below will run.
-  if (lowercase) {
-    passbase += lowers;
-  }
-
-  var uppercase = confirm("uppercase characters? ¯\\_(ツ)_/¯");
-  // If user wants uppercase characters in their password, the IF statement below will run.
-  if (uppercase) {
-    passbase += uppers;
-  }
-
-  for (let i = 0; i < passLength; i++) {
-    theChosenOne += passbase.charAt(Math.floor(Math.random()*passbase.length));
-    
-  }
-  return theChosenOne;
 }
 
 
